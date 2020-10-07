55640271091c31be45702883c91460b4967db88d
anav238 <anavasiliu139@gmail.com>
2020-10-07 14:58:19 +0300

    initial commit

---
+++ Assets/movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject img; //ImageTarget-ul
+    public GameObject duck; //Animated Object
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dist = img.transform.position; //position of target
+        float z = dist[2];
+        if (z < 50)
+        {
+            duck.GetComponent<Animator>().enabled = false;
+        }
+        else
+        {
+            duck.GetComponent<Animator>().enabled = true;
+        }
+    }
+}
